01437925331b769ab9c0752989d8c696783c4439
Danila Zinin <danilazinin@MacBook-Pro-Danila.local>
2022-08-31 23:15:39 +0300

    Внечены измененя

--- PrG1/Program.cs
+++ PrG1/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Введите 3 числа:");
 Console.WriteLine("Введите первое число:");
-int a = int.Parse(Console.ReadLine());
+int a = int .Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
-int b = int.Parse(Console.ReadLine());
+int  b = int .Parse(Console.ReadLine());
 Console.WriteLine("Введите третье число:");
-int c = int.Parse(Console.ReadLine());
+int  c = int .Parse(Console.ReadLine());
 
 int sum = a + b + c;
-int arg = sum/3;
+ double arg = sum/3;
 Console.WriteLine($"Среднее арифметическое равно {arg} ");
--- PrG3/Program.cs
+++ PrG3/Program.cs
@@ -3,7 +3,7 @@
 //5 -> Пятница
 Console.WriteLine("Введите число от 1 до 7:");
 int a = int.Parse(Console.ReadLine());
-string[] weekDays = new string[] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятницат", "Суббота", "Воскресенье" };
+string[] weekDays = new string[] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
 if (a > 0 && a < 8) {
     Console.WriteLine(weekDays[a-1]);
 }
